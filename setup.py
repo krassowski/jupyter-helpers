8ca6eb15892f5728a89c8acf517aac3daffe9cc4
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-02-10 16:18:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     setup(
         name='jupyter_helpers',
         packages=find_packages(),
-        version='0.2.2',
+        version='0.2.3',
         license='MIT',
         description='A collection of helpers for using IPython in Jupyter(Lab)',
         long_description=get_long_description('README.md'),
